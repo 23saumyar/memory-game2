fd526a96171763d05ef05cd2219af9b461d37579
Saumya Rawat <saumyar@mit.edu>
2020-01-30 03:05:24 -0500

    Updates

---
+++ precache-manifest.c3473b79ff81459063d4f508e4aa4744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629995f6f12d225cfe63b48956d92bfe",
+    "url": "/memory-game2/index.html"
+  },
+  {
+    "revision": "36c5b0bc910a5afc4b63",
+    "url": "/memory-game2/static/css/2.7752fdd6.chunk.css"
+  },
+  {
+    "revision": "7db932e6be8ab934f9f0",
+    "url": "/memory-game2/static/css/main.9cc5b899.chunk.css"
+  },
+  {
+    "revision": "36c5b0bc910a5afc4b63",
+    "url": "/memory-game2/static/js/2.b4d31414.chunk.js"
+  },
+  {
+    "revision": "cfb92c278d7b064e3608dea30d5f58ca",
+    "url": "/memory-game2/static/js/2.b4d31414.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7db932e6be8ab934f9f0",
+    "url": "/memory-game2/static/js/main.723b3bb6.chunk.js"
+  },
+  {
+    "revision": "40397ca5ba56ee9e4d48",
+    "url": "/memory-game2/static/js/runtime-main.5eb6c822.js"
+  }
+]);
