6924c1c02b54e024467c84a00f3e6605c1f9aac7
Saumya Rawat <saumyar@mit.edu>
2020-02-01 15:23:59 -0500

    Updates

---
+++ precache-manifest.d410c6c1435d46586b867e300acf5a89.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a6184def1d680f00c8304d7621535d",
+    "url": "/memory-game2/index.html"
+  },
+  {
+    "revision": "b063b01bd0506d00371a",
+    "url": "/memory-game2/static/css/2.7752fdd6.chunk.css"
+  },
+  {
+    "revision": "2866d8471fec124172b7",
+    "url": "/memory-game2/static/css/main.f36b1b8d.chunk.css"
+  },
+  {
+    "revision": "b063b01bd0506d00371a",
+    "url": "/memory-game2/static/js/2.f6ea7f0e.chunk.js"
+  },
+  {
+    "revision": "cfb92c278d7b064e3608dea30d5f58ca",
+    "url": "/memory-game2/static/js/2.f6ea7f0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2866d8471fec124172b7",
+    "url": "/memory-game2/static/js/main.ad02b170.chunk.js"
+  },
+  {
+    "revision": "40397ca5ba56ee9e4d48",
+    "url": "/memory-game2/static/js/runtime-main.5eb6c822.js"
+  },
+  {
+    "revision": "12f16f78a95bb552d9c088f396103b65",
+    "url": "/memory-game2/static/media/pause.12f16f78.png"
+  },
+  {
+    "revision": "e5046bdc177b1163e291efc381bbf9f0",
+    "url": "/memory-game2/static/media/start.e5046bdc.png"
+  },
+  {
+    "revision": "a8dd6bd758ae7d1d161d01e681b48ff9",
+    "url": "/memory-game2/static/media/stop.a8dd6bd7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/memory-game2/precache-manifest.f551367b1f7877ae89eb74ca8a48b292.js"
+  "/memory-game2/precache-manifest.d410c6c1435d46586b867e300acf5a89.js"
 );
 
 self.addEventListener('message', (event) => {
